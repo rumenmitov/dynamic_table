72936fcd0e8ee844af04a60109a912ecd813cb2d
Rumen <rumenmitov@protonmail.com>
2024-07-10 20:26:16 +0200

    added some docs

--- src/dyn_table.h
+++ src/dyn_table.h
@@ -7,21 +7,41 @@
 #include <iterator>
 #include <sys/mman.h>
 
+/* Virtual address */
 typedef size_t addr_t;
 
-enum DynTableError { FailedInit, PoolFull, FailedExt };
+/*
+ * @brief Possible throw errors of the DynTable methods.
+ *
+ * FailedInit - initialization failed
+ * FailedExt  - could not extend memory pool
+ */
+enum DynTableError { FailedInit, FailedExt };
 
+/*
+ * @brief Class for that manages virtual memory with the help of a dynamic table.
+ */
 class DynTable {
 
 public:
   static const uint16_t PAGE_SIZE = 4096;
   static const uint16_t DEFAULT_PAGE_COUNT = 1000;
 
+  /* NOTE base represesnts the start of the memory pool.
+   * This is needed for when the pool needs to be extended as the base location
+   * could change.
+   */
   addr_t base = 0x00;
 
-  DynTable(uint64_t bytes = DEFAULT_PAGE_COUNT * PAGE_SIZE) : n_pages(bytes / PAGE_SIZE) {
+
+  /*
+   * @brief Default constructor of DynTable.
+   * @param uint64_t bytes = DEFAULT_PAGE_COUNT * PAGE_SIZE The initial size of the memory pool.
+   * @throws DynTableError
+   */
+  DynTable(uint64_t bytes = DEFAULT_PAGE_COUNT * PAGE_SIZE) : _n_pages(bytes / PAGE_SIZE) {
     void *pool_init =
-      mmap(nullptr, n_pages * PAGE_SIZE, PROT_READ | PROT_WRITE,
+      mmap(nullptr, _n_pages * PAGE_SIZE, PROT_READ | PROT_WRITE,
 	   MAP_PRIVATE | MAP_ANON, 0, PAGE_SIZE);
 
     if (pool_init == MAP_FAILED)
@@ -29,18 +49,19 @@ public:
 
     base = (addr_t)pool_init;
 
-    _pool.push_back(new Block{.offset = 0, .size = n_pages, .state = Free});
-
-    _pool_partitions = 1;
+    _pool.push_back(new Block{.offset = 0, .size = _n_pages, .state = Free});
   }
 
+  /*
+   * @brief Destructor of DynTable.
+   */
   ~DynTable() {
     if (_pool.empty())
       return;
 
     assert(_pool.front() != nullptr && "Memory was corrupted!");
 
-    munmap((void *)base, n_pages * PAGE_SIZE);
+    munmap((void *)base, _n_pages * PAGE_SIZE);
 
     while (!_pool.empty()) {
       delete _pool.back();
@@ -48,6 +69,13 @@ public:
     }
   }
 
+
+  /*
+   * @brief Interface to borrow virtual memory.
+   * @param uint64_t bytes The amount of memory needed.
+   * @returns uint64_t An offset of the base address, signifying the start of the chunk.
+   * @throws DynTableError
+   */
   uint64_t get_chunk(uint64_t bytes) {
     uint64_t n_pages = (bytes / PAGE_SIZE) + 1;
 
@@ -73,7 +101,11 @@ public:
     return _pool.back()->offset;
   }
 
-  void return_chunk(uint64_t offset) {
+  /*
+   * @brief Interface to return borrowed virtual memory.
+   * @param uint64_t offset The offset from base, signifying the start of the chunk.
+   */
+  void return_chunk(uint64_t offset) noexcept {
     for (auto block_it = _pool.begin(); block_it != _pool.end(); block_it++) {
       if ((*block_it)->offset != offset) continue;
 
@@ -85,13 +117,11 @@ public:
       if ((*succ)->state == Free) {
 	(*block_it)->size += (*succ)->size;
 	_pool.remove(*succ);
-	_pool_partitions--;
       }
 
       if ((*pred)->state == Free) {
 	(*pred)->size += (*block_it)->size;
 	_pool.remove(*block_it);
-	_pool_partitions--;
       }
     }
   }
@@ -105,26 +135,35 @@ private:
     BlockState state;
   };
 
+  /* NOTE Contains all the partition blocks. */
   std::list<Block *> _pool;
-  uint64_t n_pages = 0;
-  uint64_t _pool_partitions = 0;
 
+  /* NOTE Keeps track of the total number of pages reserved.*/
+  uint64_t _n_pages = 0;
+
+
+  /*
+   * @brief Extends the memory pool. Could potentially change the base address.
+   * @param uint64_t extra_pages The amount of pages needed.
+   * @throws DynTableError
+   */
   void extend(uint64_t extra_pages) {
-    void *pool_ext = mremap((void *)base, n_pages * PAGE_SIZE,
-                            (n_pages + extra_pages) * PAGE_SIZE, MREMAP_MAYMOVE);
+    void *pool_ext = mremap((void *)base, _n_pages * PAGE_SIZE,
+                            (_n_pages + extra_pages) * PAGE_SIZE, MREMAP_MAYMOVE);
 
     if (pool_ext == MAP_FAILED)
       throw FailedExt;
 
     base = (addr_t)pool_ext;
+    _n_pages += extra_pages;
 
-    Block *last_partition = _pool.back();
+    Block *last_block = _pool.back();
 
-    if (last_partition->state == Free)
-      last_partition->size += extra_pages;
+    if (last_block->state == Free)
+      last_block->size += extra_pages;
     else {
-      _pool.push_back(new Block{.offset = last_partition->offset +
-                                          last_partition->size * PAGE_SIZE,
+      _pool.push_back(new Block{.offset = last_block->offset +
+                                          last_block->size * PAGE_SIZE,
                                 .size = extra_pages,
                                 .state = Free});
     }
